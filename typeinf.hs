9d6bdc4d5341c706fa708305f014411982b93551
Piotr Krzemiński <pio.krzeminski@gmail.com>
2013-05-28 22:31:39 +0200

    most of functions working on unification on types

--- typeinf.hs
+++ typeinf.hs
@@ -10,6 +10,10 @@ instance (Show Type) where
   show (FunType t (VarType x)) = "(" ++ (show t) ++ ")->" ++ (show x)
   show (FunType t1 t2) = "(" ++ (show t1) ++ ")->(" ++ (show t2) ++ ")"
 
+instance (Eq Type) where
+  VarType x == VarType y           = x == y
+  FunType t1 t2 == FunType u1 u2   = t1 == u1 && t2 == u2
+
 type VarName = String
 data LambdaTerm = Var VarName | App LambdaTerm LambdaTerm | Abs VarName LambdaTerm
 
@@ -26,9 +30,10 @@ type TypeContext = [(VarName, Type)]
 type TermTypeInfo = (TypeContext, Type)
 
 inferType :: LambdaTerm -> Maybe TermTypeInfo
-inferType term = case inferType' term 1 of
-                   Just (result, _) -> Just result
-                   Nothing -> Nothing
+inferType term =
+  case inferType' term 1 of
+    Just (result, _) -> Just result
+    Nothing -> Nothing
 
 inferType' :: LambdaTerm -> Int -> Maybe (TermTypeInfo, Int)
 
@@ -37,6 +42,7 @@ inferType' (Var x) newType =
     freshContext = [(x, typeOfX)]
     typeOfX = VarType newType
   in Just ((freshContext, typeOfX), newType + 1)
+
 {--
 inferType' (App m1 m2) newType
   | inferType m1 newType == Nothing -> Nothing
@@ -63,8 +69,49 @@ showType term = case inferType term of
                  Just (context, termType) -> (show term) ++ " : " ++ (show termType)
 
 
+type TypeUnificator = [(VarTypeName, Type)]
 
+applyUnificatorToType :: TypeUnificator -> Type -> Type
+applyUnificatorToType unif (VarType x) =
+  case lookup x unif of
+    Nothing -> (VarType x)
+    Just typeAssignment -> typeAssignment
+applyUnificatorToType unif (FunType t1 t2) =
+  FunType (applyUnificatorToType unif t1) (applyUnificatorToType unif t2)
 
+unifyTypes :: Type -> Type -> Maybe TypeUnificator
+unifyTypes (VarType x) (VarType y) | x == y = Just []
+unifyTypes (VarType x) t = Just [(x, t)]
+unifyTypes (FunType t _) (FunType u _) | unifyTypes t u == Nothing = Nothing
+unifyTypes (FunType _ t) (FunType _ u) | unifyTypes t u == Nothing = Nothing
+unifyTypes (FunType t1 t2) (FunType u1 u2) =
+  let
+    Just unif1 = unifyTypes t1 u1
+    Just unif2 = unifyTypes t2 u2
+  in
+    joinUnificators unif1 unif2
+unifyTypes _ _ = Nothing
+
+joinUnificators :: TypeUnificator -> TypeUnificator -> Maybe TypeUnificator
+joinUnificators unif1 unif2 = joinUnificators' unif1 unif2 []
+
+joinUnificators' :: TypeUnificator -> TypeUnificator -> TypeUnificator -> Maybe TypeUnificator
+joinUnificators' [] unif acc = Just (unif ++ acc)
+joinUnificators' ((x,t):xs) unif acc
+  | lookup x unif == Nothing = joinUnificators' xs unif ((x,t):(acc))
+  | lookup x unif == Just t = joinUnificators' xs (deleteFromSet x unif) ((x,t):(acc))
+  | otherwise = Nothing
+
+deleteFromSet :: Eq a => a -> [(a,b)] -> [(a,b)]
+deleteFromSet elem list = deleteFromSet' elem list []
+
+deleteFromSet' :: Eq a => a -> [(a,b)] -> [(a,b)] -> [(a,b)]
+deleteFromSet' _ [] acc = acc
+deleteFromSet' key ((k,_):es) acc | key == k = es ++ acc
+deleteFromSet' key (e:es) acc = deleteFromSet' key es (e:acc)
+
+--unifyContexts :: TypeContext -> TypeContext -> [VarTypeName] -> TypeUnificator
+--applyUnificatorToTypeContext :: TypeUnificator -> TypeContext -> TypeContext
 
 
 
