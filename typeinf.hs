caabf0540c9ccb44a62c0bc9f584a5f711ac88f8
Piotr Krzemiński <pio.krzeminski@gmail.com>
2013-05-30 14:01:12 +0200

    added support for multi abstraction; printing lambda terms and type info with support for greek letters

--- typeinf.hs
+++ typeinf.hs
@@ -1,6 +1,7 @@
 module TypeInf where
 
 import Data.List
+import Data.Char
 
 -- type variables for now are just integers
 type VarTypeName = Int
@@ -9,11 +10,11 @@ data Type = VarType VarTypeName | FunType Type Type
 
 -- instance of show to pretty print types
 instance (Show Type) where
-  show (VarType x) = (show x)
-  show (FunType (VarType x) (VarType y)) = (show x) ++ "->" ++ (show y)
-  show (FunType (VarType x) t) = (show x) ++ "->(" ++ (show t) ++ ")"
-  show (FunType t (VarType x)) = "(" ++ (show t) ++ ")->" ++ (show x)
-  show (FunType t1 t2) = "(" ++ (show t1) ++ ")->(" ++ (show t2) ++ ")"
+  show (VarType x) = [chr (944 + x)]
+  show (FunType (VarType x) (VarType y)) = (show (VarType x)) ++ "->" ++ (show (VarType y))
+  show (FunType (VarType x) t) = (show (VarType x)) ++ "->(" ++ (show t) ++ ")"
+  show (FunType t (VarType x)) = "(" ++ (show t) ++ ")->" ++ (show (VarType x))
+  show (FunType t1 t2) = "(" ++ (show t1) ++ ")->" ++ (show t2)
 
 -- comparing types for equality 
 instance (Eq Type) where
@@ -23,17 +24,28 @@ instance (Eq Type) where
 -- lambda term variable names
 type VarName = String
 -- lambda terms are variables, applications or lambda abstractions
-data LambdaTerm = Var VarName | App LambdaTerm LambdaTerm | Abs VarName LambdaTerm
+data LambdaTerm = Var VarName |
+                  App LambdaTerm LambdaTerm |
+                  Abs VarName LambdaTerm |
+                  MultiAbs [VarName] LambdaTerm
 
 -- instance of show to pretty print lambda terms
 instance (Show LambdaTerm) where
   show (Var m) = m
-  show (App (Var x) (Var y)) = x ++ y
-  show (App (Var x) m) = x ++ "(" ++ (show m) ++ ")"
-  show (App m (Var x)) = "(" ++ (show m) ++ ")" ++ x
-  show (App m1 m2) = "(" ++ (show m1) ++ ")" ++ "(" ++ (show m2) ++ ")"
-  show (Abs x (Var y)) = "\\" ++ x ++ "." ++ y
-  show (Abs x m) = "\\" ++ x ++ ".(" ++ (show m) ++ ")"
+  show (App (Var x) (Var y)) = x ++ " " ++ y
+  show (App (Var x) m) = x ++ " (" ++ (show m) ++ ")"
+  show (App m (Var x)) = (show m) ++ " " ++ x
+  show (App m1 m2) = "(" ++ (show m1) ++ ") (" ++ (show m2) ++ ")"
+  show (Abs x (Var y)) = "\955" ++ x ++ "." ++ y
+  show (Abs x m) = "\955" ++ x ++ "." ++ (show m)
+  show (MultiAbs [x] m) = show (Abs x m)
+  show (MultiAbs xs m) = "\955" ++ (drop 1 $ foldl (\a b -> a ++ " " ++ b) "" xs) ++ "." ++ (show m) 
+
+-- rebuild multi abstraction as normal abstraction tree
+multiAbsToAbs :: LambdaTerm -> LambdaTerm
+multiAbsToAbs (MultiAbs [x] m) = Abs x m
+multiAbsToAbs (MultiAbs (x:xs) m) = Abs x (multiAbsToAbs (MultiAbs xs m))
+multiAbsToAbs term = term
 
 -- type context is mapping from variable name to type
 type TypeContext = [(VarName, Type)]
@@ -101,6 +113,11 @@ inferType' (Abs x m) newType
       case lookup x mTypeContext of
         Nothing -> Just ((mTypeContext, (FunType (VarType newType') mType)), newType' + 1)
         Just xType -> Just (((deleteFromSet x mTypeContext), (FunType xType mType)), newType')
+
+-- type infering of multi abstraction is rebuilding it as normal abstraction first
+inferType' (MultiAbs xs m) newType =
+  let abs = multiAbsToAbs (MultiAbs xs m)
+  in inferType' abs newType
 
 reenumerateTermTypeInfo :: TermTypeInfo -> TermTypeInfo
 reenumerateTermTypeInfo (context, termType) =
